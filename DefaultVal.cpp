a19ebbb51793511c6243a1e66ca07597179c9b97
28kayak <kaya_ota@msn.com>
2013-03-21 18:52:12 -0700

    commented

--- DefaultVal.cpp
+++ DefaultVal.cpp
@@ -11,8 +11,12 @@ void display(int i, int x = 0, int y = 0)
 int main()
 {
 	display(1);
+	//because x and y are not declared, 
+	//this program uses defalt value(x= 0, y =0) 
 	display(2,1);
+	//y are not declared, this program assumes that y = 0
 	display(3,2,3);
+	//all variables are declared.
 	
 	return 0;
 }
